d66781bddb0a50e7de9fbcb3d5299d41c7f56cb9
Jacob Madeley <jacob.madeley@my.jcu.edu.au>
2024-02-28 20:10:23 +1000

    Update views.py

--- hotel_booking_app/views.py
+++ hotel_booking_app/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+
 
 # Create your views here.
 def dashboard(request):
-    template = loader.get_template('dashboard.html')
-    return HttpResponse(template.render())
-    # return render(request, "dashboard.html")
+    return render(request, "dashboard.html")
 
 
 def booking(request):
