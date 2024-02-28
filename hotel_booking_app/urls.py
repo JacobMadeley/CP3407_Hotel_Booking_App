3a809f7454c38fff48b834296154f526ae552f40
Jacob Madeley <jacob.madeley@my.jcu.edu.au>
2024-02-28 19:38:05 +1000

    Update urls.py

--- hotel_booking_app/urls.py
+++ hotel_booking_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [ 
     path("", views.dashboard, name="dashboard"), 
     path("booking/", views.booking, name="booking"),
-    path("user/", views.user, name="user")
+    path("user/", views.user, name="user"),
     path("contact/", views.contact, name="contact"), 
 ]
