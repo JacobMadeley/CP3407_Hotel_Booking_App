dddcb7fe4a22ee4a6b5ef98a3462155419c2db39
Jacob Madeley <jacob.madeley@my.jcu.edu.au>
2024-02-28 19:31:02 +1000

    Update views.py

--- hotel_booking_app/views.py
+++ hotel_booking_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, "dashboard.html")
+
+
+def booking(request):
+    return render(request, "booking.html")
+
+
+def users(request):
+    return render(request, "users.html")
+
+
+def contacts(request):
+  return render(request, "contact.html")
