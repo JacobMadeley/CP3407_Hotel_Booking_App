265e6f3fd9db997692a9fffa32cc61e9d4154fd7
Jacob Madeley <jacob.madeley@my.jcu.edu.au>
2024-03-20 19:46:09 +1000

    add bookings to models.py

--- hotel_booking_app/admin.py
+++ hotel_booking_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Hotel, EmployeeRole, Employee, Booking, Guest, Room, RoomType, Payment
+from .models import Hotel, EmployeeRole, Employee, Booking, Guest, Inventory, Room, RoomType, Payment
 
 admin.site.register(Hotel)
 admin.site.register(EmployeeRole)
 admin.site.register(Employee)
 admin.site.register(Booking)
 admin.site.register(Guest)
+admin.site.register(Inventory)
 admin.site.register(Room)
 admin.site.register(RoomType)
 admin.site.register(Payment)
