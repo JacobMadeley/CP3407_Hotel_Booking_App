57c8459edac300261680c6c4aff7cd0d7ab67bdf
Louis-Bui <louis.bui@my.jcu.edu.au>
2024-04-12 21:08:41 +1000

    More css update

--- hotel_booking_app/forms.py
+++ hotel_booking_app/forms.py
@@ -8,26 +8,25 @@ from .models import Booking
 from .models import Guest
 from .models import Room
 from django.utils import timezone
-# from .models import Record
 
 
 class BookingForm(forms.Form):
-    start_date = forms.DateField(label='start date', initial=datetime.date.today)
-    end_date = forms.DateField(label='end date', initial=datetime.date.today)
-    adults_number = forms.IntegerField(label='Adults', max_value=10)
-    children_number = forms.IntegerField(label='Children', max_value=10)
-    guest_title = forms.CharField(label= 'title',max_length=10)
-    guest_first_name = forms.CharField(label='first name',max_length=50)
-    guest_last_name = forms.CharField(label='last name',max_length=50)
-    guest_date_of_birth = forms.DateField(label='date of birth',initial=timezone.now)
-    guest_phone_number = forms.CharField(label='phone number',max_length=20)
-    guest_email = forms.EmailField(label='email')
-    guest_address = forms.CharField(label='address',max_length=50)
-    guest_city = forms.CharField(label='city',max_length=50)
-    guest_state = forms.CharField(label='state',max_length=50)
-    guest_country = forms.CharField(label='country',max_length=50)
-    guest_postcode = forms.CharField(label='postcode',max_length=10)
-    room = forms.ModelChoiceField(label= 'room', queryset=Room.objects, required=True)
+    start_date = forms.DateField(label='Start Date', initial=datetime.date.today,widget=forms.DateInput(attrs={"class": "form-control"}))
+    end_date = forms.DateField(label='End Date', initial=datetime.date.today,widget=forms.DateInput(attrs={"class": "form-control"}))
+    adults_number = forms.IntegerField(label='Adults', max_value=10,widget=forms.NumberInput(attrs={"class": "form-control"}))
+    children_number = forms.IntegerField(label='Children', max_value=10,widget=forms.NumberInput(attrs={"class": "form-control"}))
+    guest_title = forms.CharField(label= 'Title',max_length=10,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_first_name = forms.CharField(label='First Name',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_last_name = forms.CharField(label='Last Name',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_date_of_birth = forms.DateField(label='Date of Birth',initial=timezone.now,widget=forms.DateInput(attrs={"class": "form-control"}))
+    guest_phone_number = forms.CharField(label='Phone Number',max_length=20,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={"class": "form-control"}))
+    guest_address = forms.CharField(label='Address',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_city = forms.CharField(label='City',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_state = forms.CharField(label='State',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_country = forms.CharField(label='Country',max_length=50,widget=forms.TextInput(attrs={"class": "form-control"}))
+    guest_postcode = forms.CharField(label='Postcode',max_length=10,widget=forms.TextInput(attrs={"class": "form-control"}))
+    room = forms.ModelChoiceField(label= 'Room', queryset=Room.objects, required=True,widget=forms.Select(attrs={"class": "form-control"}))
 
 
 class AddRoomType(forms.Form):
@@ -38,10 +37,11 @@ class AddRoomType(forms.Form):
     
     
 class AddRoom(forms.Form):
-    room_number = forms.IntegerField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Room Number", "class":"form-control"}), label="",error_messages={'required': ''})
-    room_status = forms.ChoiceField(required=True, label='Room Status', choices=[(1,'Occupied'), (2,'Free'),(3,'Needs Cleaning'), (4,'Needs Maintenance')], widget=forms.Select(attrs={"class": "form-control"}))
-    hotel = forms.ModelChoiceField(label= 'Hotel', queryset=Hotel.objects, required=False, widget=forms.Select(attrs={"class": "form-control"}))
-    room_type = forms.ModelChoiceField(label='Room Type', queryset=RoomType.objects, required=False, widget=forms.Select(attrs={"class": "form-control"}))
+    room_number = forms.IntegerField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Room Number", "class":"form-control"}),label="",error_messages={'required': ''})
+    room_status = forms.ChoiceField(required=True, label='Room Status', choices=[(1,'Occupied'), (2,'Free'),(3,'Needs Cleaning'), (4,'Needs Maintenance')],widget=forms.Select(attrs={"class": "form-control"}))
+    hotel = forms.ModelChoiceField(label= 'Hotel', queryset=Hotel.objects, required=False,widget=forms.Select(attrs={"class": "form-control"}))
+    room_type = forms.ModelChoiceField(label='Room Type', queryset=RoomType.objects, required=False,widget=forms.Select(attrs={"class": "form-control"}))
+
 
 class PaymentForm(forms.Form):
     payment_date = forms.DateField(label='date',initial=timezone.now)
@@ -57,18 +57,7 @@ class PaymentForm(forms.Form):
     guest = forms.ModelChoiceField(label= 'guest', queryset = Guest.objects, disabled=True,widget=forms.HiddenInput())
     
 
-
 class SignUpForm(UserCreationForm):
-	# email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-	# first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-	# last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
-
-
-	# class Meta:
-	# 	model = User
-	# 	fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-
-
 	def __init__(self, *args, **kwargs):
 		super(SignUpForm, self).__init__(*args, **kwargs)
 
@@ -88,20 +77,7 @@ class SignUpForm(UserCreationForm):
 		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
 
 
-
-
 class AddRecordForm(forms.ModelForm):
-	# first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="")
-	# last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="")
-	# email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), label="")
-	# phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"form-control"}), label="")
-	# address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), label="")
-	# city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), label="")
-	# state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}), label="")
-	# zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), label="")
-
-
-
     hotel_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Hotel Name", "class":"form-control"}), label="",error_messages={'required': ''})
     hotel_address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Hotel Address", "class":"form-control"}), label="",error_messages={'required': ''})
     hotel_city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Hotel City", "class":"form-control"}), label="",error_messages={'required': ''})
@@ -116,5 +92,5 @@ class AddRecordForm(forms.ModelForm):
     
 
 class UpdateRoomStatus(forms.Form):
-    room_number = forms.IntegerField(required=True,label='room number')
-    room_status = forms.ChoiceField(label='room status', choices=[(1,'occupied'), (2,'free'),(3,'needs cleaning'), (4,'needs maintenance')])
+    room_number = forms.IntegerField(required=True,widget=forms.widgets.TextInput(attrs={"placeholder":"Room Number", "class":"form-control"}), label="",error_messages={'required': ''})
+    room_status = forms.ChoiceField(required=True, label='Room Status', choices=[(1,'Occupied'), (2,'Free'),(3,'Needs Cleaning'), (4,'Needs Maintenance')], widget=forms.Select(attrs={"class": "form-control"}))
